8f6f17c05b37ca56d37a7b90aa8735b0b0ce770a
Canya04 <127889445+Canya04@users.noreply.github.com>
2023-12-14 17:22:39 +0100

    Add files via upload

---
+++ Projecte Kanban/Afegir.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Projecte_Kanban
+{
+    /// <summary>
+    /// Lógica de interacción para Afegir.xaml
+    /// </summary>
+    public partial class Afegir : Window
+    {
+        public Afegir()
+        {
+            InitializeComponent();
+        }
+
+        private void nomTasca_GotFocus(object sender, RoutedEventArgs e)
+        {            
+            nomTasca.Text = string.Empty;
+        }
+
+        private void descripcioTasca_GotFocus(object sender, RoutedEventArgs e)
+        {
+            descripcioTasca.Text = string.Empty;
+        }
+
+        private void dataTasca_GotFocus(object sender, RoutedEventArgs e)
+        {
+            dataTasca.Text = string.Empty;
+        }
+
+        private void responsableTasca_GotFocus(object sender, RoutedEventArgs e)
+        {
+            responasbleTasca.Text = string.Empty;
+        }
+    }
+}
---
+++ Projecte Kanban/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Projecte_Kanban
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection laMevaConnexioSQL;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string laMevaConnexio = ConfigurationManager.ConnectionStrings["Projecte Kanban.Properties.Settings.SimuladorKanbanConnectionString"].ConnectionString;
+
+        }
+        private void Afegir_Click(object sender, RoutedEventArgs e)
+        {
+            Afegir finestraAfegir = new Afegir();
+            finestraAfegir.ShowDialog();
+
+        }
+    }
+}
